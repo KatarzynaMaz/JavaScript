733e2a4a4fb827324ae9848abd9f9e3eca4d4a6f
Katarzyna Mazur <mazur66@hotmail.com>
2020-05-14 13:55:23 -0400

    Added a Title and instructions for the user making the shape to be a circle at least 50% of the time, added min width to the shape and added a little styling to the page

--- 05-12/app.js
+++ 05-12/app.js
@@ -1,3 +1,11 @@
+/*We were to create an application here to test our reactions
+Write a code for randomly apprearing circles or squares. When page is loaded, timer starts.
+A shape apprears, user clicks on the shape and timer counts the user's time.
+Shapes need to be of a random color and they need to randomly apprear
+on the page
+*/
+
+
 //start the timer immediately after the page loads
 
 let start = new Date().getTime();
@@ -14,15 +22,24 @@ function getRandomColor() {
 function makeShapeAppear(){
     let top = Math.random() * 400;
     let left = Math.random() * 400;
-    let width = Math.random() * 400;
+    //adding min width to width, so the shape is not too small to see
+    let width = (Math.random() * 300) + 100;
     let height = Math.random() * 400;
-    //changing a background color of a shape
-    document.getElementById("shape").style.backgroundColor = getRandomColor();
-    //making shape appear randomly vertically and horizonally by adding margins
-    document.getElementById("shape").style.top = top + "px";
-    document.getElementById("shape").style.left = left + "px";
-    document.getElementById("shape").style.width = width + "px";
-    document.getElementById("shape").style.height = height + "px";
+
+    //making the shape to apprear as circle at least 50% of the time
+        if(Math.random()>0.5) {
+            document.getElementById("shape").style.borderRadius = "50%"; 
+        } else {
+            document.getElementById("shape").style.borderRadius = "0";
+        }
+
+        //changing a background color of a shape
+        document.getElementById("shape").style.backgroundColor = getRandomColor();
+        //making shape appear randomly vertically and horizonally by adding margins
+        document.getElementById("shape").style.top = top + "px";
+        document.getElementById("shape").style.left = left + "px";
+        document.getElementById("shape").style.width = width + "px";
+        document.getElementById("shape").style.height = height + "px";
 
     document.getElementById("shape").style.display ="block";
         // update the time when the shape appears, we can use it because the start variable
